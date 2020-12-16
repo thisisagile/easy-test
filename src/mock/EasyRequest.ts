a9bbb4723546adb8ea54742f18857738fae09e5f
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-16 12:54:41 +0100

    fix(Typed EasyRequest.ts): Added types Json, JsonValue and Id to EasyRequest.ts.

--- src/mock/EasyRequest.ts
+++ src/mock/EasyRequest.ts
@@ -1,9 +1,11 @@
-type EasyRequest = { path?: any, id?: string | number, query?: any, q?: any, body?: any };
+import { Id, Json, JsonValue } from '../utils/Types';
+
+type EasyRequest = { path?: Json; id?: Id; query?: Json; q?: JsonValue; body?: Json };
 
 export const req = {
-  id: (id: string | number): EasyRequest => req.all({ path: { id } }),
-  q: (q: unknown): EasyRequest => req.all({ query: { q } }),
-  path: (path: any): EasyRequest => req.all({ path }),
-  query: (query: any): EasyRequest => req.all({ query }),
-  all: ({ path = {}, query = {}, body = {} }: {path?: any, query?: any, body?: any}): EasyRequest => ({ path, id: path.id, query, q: query.q, body }),
+  id: (id: Id): EasyRequest => req.all({ path: { id } }),
+  q: (q: JsonValue): EasyRequest => req.all({ query: { q } }),
+  path: (path: Json): EasyRequest => req.all({ path }),
+  query: (query: Json): EasyRequest => req.all({ query }),
+  all: ({ path = {}, query = {}, body = {} }: { path?: Json; query?: any; body?: Json }): EasyRequest => ({ path, id: path.id as Id, query, q: query.q, body }),
 };
--- test/mock/EasyRequest.test.ts
+++ test/mock/EasyRequest.test.ts
@@ -2,24 +2,23 @@ import { req } from '../../src';
 import { fits } from '../../dist';
 
 describe('EasyRequest', () => {
-
-  test("id", () => {
+  test('id', () => {
     const r = req.id(42);
-    expect(r).toMatchObject(fits.with({path: {id: 42}, id: 42}));
+    expect(r).toMatchObject(fits.with({ path: { id: 42 }, id: 42 }));
   });
 
-  test("q", () => {
+  test('q', () => {
     const r = req.q(42);
-    expect(r).toMatchObject(fits.with({query: {q: 42}, q: 42}));
+    expect(r).toMatchObject(fits.with({ query: { q: 42 }, q: 42 }));
   });
 
-  test("path", () => {
-    const r = req.path({ id: 42, name: "Jeroen" });
-    expect(r).toMatchObject(fits.with({path: {id: 42, name: "Jeroen"}}));
+  test('path', () => {
+    const r = req.path({ id: 42, name: 'Jeroen' });
+    expect(r).toMatchObject(fits.with({ path: { id: 42, name: 'Jeroen' } }));
   });
 
-  test("query", () => {
-    const r = req.query({ q: 42, name: "Jeroen" });
-    expect(r).toMatchObject(fits.with({query: { q: 42, name: "Jeroen" }}));
+  test('query', () => {
+    const r = req.query({ q: 42, name: 'Jeroen' });
+    expect(r).toMatchObject(fits.with({ query: { q: 42, name: 'Jeroen' } }));
   });
 });
