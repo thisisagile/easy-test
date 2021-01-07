8ab9c05d1870a6cb6b374c8f13b3546761030e52
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-07 15:47:33 +0100

    feat(Added Req.ts): Added Req class to allow "mocking" of Req request classes.

---
+++ src/utils/Req.ts
@@ -0,0 +1,23 @@
+import { Id, Json, JsonValue } from './Types';
+
+export class Req {
+  constructor(readonly state: any = {}) {}
+
+  get id(): Id {
+    return this.state.id ?? this.path.id;
+  }
+
+  get q(): JsonValue {
+    return this.state.q ?? this.query.q;
+  }
+
+  get path(): Json {
+    return this.state.path;
+  }
+
+  get query(): Json {
+    return this.state.query;
+  }
+
+  get = (key: unknown): any => this.state[key.toString()] ?? this.path[key.toString()] ?? this.query[key.toString()];
+}
