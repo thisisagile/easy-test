dea6df3b109ebba0a83ee779f732d7c0831e95b3
woutervanbakel <wouter.vb@gmail.com>
2021-05-19 16:04:48 +0200

    fix(utils): add optional body (as restResult) to Response

--- src/utils/Response.ts
+++ src/utils/Response.ts
@@ -1,3 +1,8 @@
 export type HttpStatus = { name: string; id: number };
 
-export type Response = { status: HttpStatus; headers?: { [key: string]: any } };
+export type RestResult = {
+  data?: { code: number; items: []; itemCount: number };
+  error?: { code: number; message: string; errorCount: number; errors: [] };
+};
+
+export type Response = { status: HttpStatus; headers?: { [key: string]: any }; body?: RestResult };
