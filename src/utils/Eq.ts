8d972a85f11310ee5ab66ed47c7f70db3bb4a83c
aahoogendoorn <aahoogendoorn@gmail.com>
2021-05-20 22:19:30 +0200

    fix: update to eq.

--- src/utils/Eq.ts
+++ src/utils/Eq.ts
@@ -3,20 +3,8 @@ import { iterableEquality, subsetEquality } from 'expect/build/utils';
 
 export const asString = (s?: unknown): string => ((s as any)?.toString ? (s as any).toString() : undefined);
 
-export class Eq {
-  constructor(private a?: unknown, private b?: unknown) {}
-
-  get exact(): boolean {
-    return equals(this.a, this.b, []);
-  }
-
-  get subset(): boolean {
-    return equals(this.a, this.b, [iterableEquality, subsetEquality]);
-  }
-
-  get string(): boolean {
-    return asString(this.a) === asString(this.b);
-  }
-}
-
-export const eq = (a?: unknown, b?: unknown): Eq => new Eq(a, b);
+export const eq = {
+  exact: (a?: unknown, b?: unknown): boolean => equals(a, b, []),
+  subset: (a?: unknown, b?: unknown): boolean => equals(a, b, [iterableEquality, subsetEquality]),
+  string: (a?: unknown, b?: unknown): boolean => asString(a) === asString(b),
+};
--- test/utils/Eq.test.ts
+++ test/utils/Eq.test.ts
@@ -2,122 +2,122 @@ import { eq } from '../../src/utils/Eq';
 
 describe('eq.string', () => {
   test('string false', () => {
-    expect(eq('').string).toBeFalsy();
-    expect(eq('', 'Sander').string).toBeFalsy();
-    expect(eq('Jeroen', 'Sander').string).toBeFalsy();
-    expect(eq({}, 'Sander').string).toBeFalsy();
-    expect(eq(3, 'Sander').string).toBeFalsy();
-    expect(eq(3, 5).string).toBeFalsy();
+    expect(eq.string('')).toBeFalsy();
+    expect(eq.string('', 'Sander')).toBeFalsy();
+    expect(eq.string('Jeroen', 'Sander')).toBeFalsy();
+    expect(eq.string({}, 'Sander')).toBeFalsy();
+    expect(eq.string(3, 'Sander')).toBeFalsy();
+    expect(eq.string(3, 5)).toBeFalsy();
   });
 
   test('string true', () => {
-    expect(eq().string).toBeTruthy();
-    expect(eq(3, 3).string).toBeTruthy();
-    expect(eq(3, '3').string).toBeTruthy();
-    expect(eq('Sander', 'Sander').string).toBeTruthy();
+    expect(eq.string()).toBeTruthy();
+    expect(eq.string(3, 3)).toBeTruthy();
+    expect(eq.string(3, '3')).toBeTruthy();
+    expect(eq.string('Sander', 'Sander')).toBeTruthy();
     // Don't use string for objects :)
-    expect(eq({}, { id: 3 }).string).toBeTruthy();
+    expect(eq.string({}, { id: 3 })).toBeTruthy();
   });
 });
 
 describe('eq.sub', () => {
   test('sub false', () => {
-    expect(eq('').subset).toBeFalsy();
-    expect(eq('', 'Sander').subset).toBeFalsy();
-    expect(eq('Jeroen', 'Sander').subset).toBeFalsy();
-    expect(eq({}, 'Sander').subset).toBeFalsy();
-    expect(eq(3, 'Sander').subset).toBeFalsy();
-    expect(eq(3, 5).subset).toBeFalsy();
-    expect(eq(3, '3').subset).toBeFalsy();
-    expect(eq({}, { id: 3 }).subset).toBeFalsy();
-    expect(eq({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Wouter' }).subset).toBeFalsy();
+    expect(eq.subset('')).toBeFalsy();
+    expect(eq.subset('', 'Sander')).toBeFalsy();
+    expect(eq.subset('Jeroen', 'Sander')).toBeFalsy();
+    expect(eq.subset({}, 'Sander')).toBeFalsy();
+    expect(eq.subset(3, 'Sander')).toBeFalsy();
+    expect(eq.subset(3, 5)).toBeFalsy();
+    expect(eq.subset(3, '3')).toBeFalsy();
+    expect(eq.subset({}, { id: 3 })).toBeFalsy();
+    expect(eq.subset({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Wouter' })).toBeFalsy();
     expect(
-      eq(
+      eq.subset(
         { id: 3, name: { first: 'Naoufal', last: 'Lamarti' } },
         {
           id: 3,
           name: { first: 'Wouter', last: 'Lamarti' },
         }
-      ).subset
+      )
     ).toBeFalsy();
   });
 
   test('sub true', () => {
-    expect(eq().subset).toBeTruthy();
-    expect(eq(3, 3).subset).toBeTruthy();
-    expect(eq('Sander', 'Sander').subset).toBeTruthy();
-    expect(eq({ id: 3 }, { id: 3 }).subset).toBeTruthy();
-    expect(eq({ id: 3, name: 'Naoufal' }, {}).subset).toBeTruthy();
-    expect(eq({ id: 3, name: 'Naoufal' }, { id: 3 }).subset).toBeTruthy();
-    expect(eq({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Naoufal' }).subset).toBeTruthy();
+    expect(eq.subset()).toBeTruthy();
+    expect(eq.subset(3, 3)).toBeTruthy();
+    expect(eq.subset('Sander', 'Sander')).toBeTruthy();
+    expect(eq.subset({ id: 3 }, { id: 3 })).toBeTruthy();
+    expect(eq.subset({ id: 3, name: 'Naoufal' }, {})).toBeTruthy();
+    expect(eq.subset({ id: 3, name: 'Naoufal' }, { id: 3 })).toBeTruthy();
+    expect(eq.subset({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Naoufal' })).toBeTruthy();
     expect(
-      eq(
+      eq.subset(
         { id: 3, name: { first: 'Naoufal', last: 'Lamarti' } },
         {
           id: 3,
           name: { last: 'Lamarti' },
         }
-      ).subset
+      )
     ).toBeTruthy();
     expect(
-      eq(
+      eq.subset(
         { id: 3, name: { first: 'Naoufal', last: 'Lamarti' } },
         {
           id: 3,
           name: { first: 'Naoufal', last: 'Lamarti' },
         }
-      ).subset
+      )
     ).toBeTruthy();
   });
 });
 
 describe('eq.full', () => {
   test('full false', () => {
-    expect(eq('').exact).toBeFalsy();
-    expect(eq('', 'Sander').exact).toBeFalsy();
-    expect(eq('Jeroen', 'Sander').exact).toBeFalsy();
-    expect(eq({}, 'Sander').exact).toBeFalsy();
-    expect(eq(3, 'Sander').exact).toBeFalsy();
-    expect(eq(3, 5).exact).toBeFalsy();
-    expect(eq(3, '3').exact).toBeFalsy();
-    expect(eq({}, { id: 3 }).exact).toBeFalsy();
-    expect(eq({ id: 3, name: 'Naoufal' }, {}).exact).toBeFalsy();
-    expect(eq({ id: 3, name: 'Naoufal' }, { id: 3 }).exact).toBeFalsy();
-    expect(eq({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Wouter' }).exact).toBeFalsy();
+    expect(eq.exact('')).toBeFalsy();
+    expect(eq.exact('', 'Sander')).toBeFalsy();
+    expect(eq.exact('Jeroen', 'Sander')).toBeFalsy();
+    expect(eq.exact({}, 'Sander')).toBeFalsy();
+    expect(eq.exact(3, 'Sander')).toBeFalsy();
+    expect(eq.exact(3, 5)).toBeFalsy();
+    expect(eq.exact(3, '3')).toBeFalsy();
+    expect(eq.exact({}, { id: 3 })).toBeFalsy();
+    expect(eq.exact({ id: 3, name: 'Naoufal' }, {})).toBeFalsy();
+    expect(eq.exact({ id: 3, name: 'Naoufal' }, { id: 3 })).toBeFalsy();
+    expect(eq.exact({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Wouter' })).toBeFalsy();
     expect(
-      eq(
+      eq.exact(
         { id: 3, name: { first: 'Naoufal', last: 'Lamarti' } },
         {
           id: 3,
           name: { last: 'Lamarti' },
         }
-      ).exact
+      )
     ).toBeFalsy();
     expect(
-      eq(
+      eq.exact(
         { id: 3, name: { first: 'Naoufal', last: 'Lamarti' } },
         {
           id: 3,
           name: { first: 'Wouter', last: 'Lamarti' },
         }
-      ).exact
+      )
     ).toBeFalsy();
   });
 
   test('full true', () => {
-    expect(eq().exact).toBeTruthy();
-    expect(eq(3, 3).exact).toBeTruthy();
-    expect(eq('Sander', 'Sander').exact).toBeTruthy();
-    expect(eq({ id: 3 }, { id: 3 }).exact).toBeTruthy();
-    expect(eq({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Naoufal' }).exact).toBeTruthy();
+    expect(eq.exact()).toBeTruthy();
+    expect(eq.exact(3, 3)).toBeTruthy();
+    expect(eq.exact('Sander', 'Sander')).toBeTruthy();
+    expect(eq.exact({ id: 3 }, { id: 3 })).toBeTruthy();
+    expect(eq.exact({ id: 3, name: 'Naoufal' }, { id: 3, name: 'Naoufal' })).toBeTruthy();
     expect(
-      eq(
+      eq.exact(
         { id: 3, name: { first: 'Naoufal', last: 'Lamarti' } },
         {
           id: 3,
           name: { first: 'Naoufal', last: 'Lamarti' },
         }
-      ).exact
+      )
     ).toBeTruthy();
   });
 });
