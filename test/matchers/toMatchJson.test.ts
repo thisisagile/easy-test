9b77ad4bd06a926c30c11aaa7a1369435a2b7da7
aahoogendoorn <aahoogendoorn@gmail.com>
2021-05-20 23:27:35 +0200

    test(matchers): updated toMatchJson.test.ts

--- test/matchers/toMatchJson.test.ts
+++ test/matchers/toMatchJson.test.ts
@@ -1,13 +1,20 @@
 import { MatchesJson, toMatchJson } from '../../src';
 
-describe('toMatchJson', () => {
-  class Dev {
-    constructor(readonly name: string) {}
-  }
+class Dev {
+  constructor(readonly name: string) {}
+}
 
+class DevWithFunction {
+  constructor(readonly name: string) {}
+
+  initials = (): string => this.name[0];
+}
+
+describe('toMatchJson', () => {
   const json = { name: 'Sander', id: 3 };
   const dev = new Dev('Sander');
   const devWrong = new Dev('Wouter');
+  const devFunction = new DevWithFunction('Wouter');
 
   test('fails', () => {
     expect(toMatchJson()).toFailMatcherWith(MatchesJson.SubjectUndefined);
@@ -15,11 +22,14 @@ describe('toMatchJson', () => {
     expect(toMatchJson(json, 'Hi')).toFailMatcherWith(MatchesJson.DoesNotMatch);
     expect(toMatchJson(json, devWrong)).toFailMatcherWith(MatchesJson.DoesNotMatch);
     expect(toMatchJson(dev, json)).toFailMatcherWith(MatchesJson.DoesNotMatch);
+    expect(toMatchJson(dev, devFunction)).toFailMatcherWith(MatchesJson.DoesNotMatch);
   });
 
   test('passes', () => {
     expect(toMatchJson(json, {})).toPassMatcherWith(MatchesJson.Yes);
     expect(toMatchJson(json, json)).toPassMatcherWith(MatchesJson.Yes);
     expect(toMatchJson(json, dev)).toPassMatcherWith(MatchesJson.Yes);
+    expect(toMatchJson(devFunction, devFunction)).toPassMatcherWith(MatchesJson.Yes);
+    expect(toMatchJson(devFunction, devWrong)).toPassMatcherWith(MatchesJson.Yes);
   });
 });
