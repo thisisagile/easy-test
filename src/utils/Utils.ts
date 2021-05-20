3eba5c3afb34e29b23b7118f19f6e31dc8e8d769
aahoogendoorn <aahoogendoorn@gmail.com>
2021-05-20 23:07:12 +0200

    fix: minor additions to Utils (from easy).

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -4,3 +4,8 @@ export const isFunction = (o?: unknown): o is (...params: unknown[]) => unknown 
 
 export const isA = <T>(t?: unknown, ...properties: (keyof T)[]): t is T => isDefined(t) && properties.every(p => isDefined((t as T)[p]));
 export const isAn = isA;
+
+export const isArray = <T = any>(o?: unknown): o is Array<T> => isDefined(o) && o instanceof Array;
+export const isObject = (o?: unknown): o is Record<string, unknown> => o != null && (typeof o === 'object' || typeof o === 'function') && !isArray(o);
+
+export const asJson = (a?: unknown): any => ((a as any)?.toJSON ? (a as any).toJSON() : isObject(a) ? a : undefined);
